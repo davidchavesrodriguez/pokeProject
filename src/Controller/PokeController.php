8fb0b9b7951f08f36770191c334b5cd260a811f2
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-03-05 19:37:28 +0100

    todo

--- src/Controller/PokeController.php
+++ src/Controller/PokeController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Pokemon; // Import the Pokemon entity
+use App\Entity\Pokemon;
 use App\Repository\PokemonRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
